5fb07574d39fe8293c805ed6879888bce010b6cf
Joao Esperancinha <joao.esperancinha@eonics.nl>
2021-04-07 14:22:20 +0200

    🐢 🐇 - Brent's’s Cycle-Finding Algorithm - Javascript

--- jeorg-javascript-algorithms/jeorg-javascript-alg2-floyd-tortoise-hare/app.js
+++ jeorg-javascript-algorithms/jeorg-javascript-alg2-floyd-tortoise-hare/app.js
@@ -47,5 +47,16 @@ let floyd = (function_data) => {
     };
 }
 
+let before = new Date();
+console.log(before)
 console.log(floyd([2, 0, 6, 3, 1, 6, 3, 1, 6, 3, 1]));
 console.log(floyd([1, 2, 3, 4, 5, 6, 5, 6, 5, 6, 5, 6, 5, 6, 6, 7, 5, 3, 4, 2, 4, 5, 7]));
+for (let i = 0; i < 1000; i++) {
+    console.log(floyd(
+        [1, 2, 3, 4, 5, 6, 5, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6,
+            5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6, 6, 5, 6,
+            6, 5, 6, 5, 6, 5, 6, 6, 7, 5, 3, 4, 2, 4, 5, 7]))
+}
+let after = new Date();
+console.log(after)
+console.log(after - before)
